fb9b18a75824116b8b8fc4517d3631a215736432
Eduards-UC <43106401@continental.edu.pe>
2025-11-08 12:48:52 -0500

    Update Ejercicio2_WHILE.asm

--- Ejercicio2_WHILE.asm
+++ Ejercicio2_WHILE.asm
@@ -1,1 +1,38 @@
+; ----------------------------------------
+; Ejercicio 2: WHILE (i <= n)
+; Calcula una serie con sumas acumuladas
+; ----------------------------------------
+
+.MODEL SMALL
+.STACK 100H
+.DATA
+n DB 5
+fant DB 1
+f DB 1
+i DB 2
+faux DB ?
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+WHILE_LOOP:
+    MOV AL, i
+    CMP AL, n
+    JA END_WHILE       ; Si i > n → fin del bucle
+
+    MOV AL, f
+    MOV faux, AL
+    ADD AL, fant
+    MOV f, AL
+    MOV AL, faux
+    MOV fant, AL
+    INC i
+    JMP WHILE_LOOP
+
+END_WHILE:
+    MOV AH, 4CH
+    INT 21H
+MAIN ENDP
+END MAIN
 
